08d39d8436fd3fc4ab6c4d70bc54e4ae6816e004
Nikolay.Walters <zcapnwa@ucl.ac.uk>
2017-10-04 12:08:56 +0100

    Displays date and time

---
+++ PHAS3459/src/module1/Simple2.java
@@ -0,0 +1,10 @@
+package module1;
+import java.util.Date;
+public class Simple2 {
+ public static void main(String[] args) {
+ System.out.println("Program starting");
+ Date myDate = new Date();
+ System.out.println(myDate);
+ System.out.println("Program finished");
+ }
+}
